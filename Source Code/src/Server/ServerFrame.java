2be371a1e909209eea58f14e6f9ddf3717dbee09
Andrew Smith <andrw.l.smth@gmail.com>
2017-03-31 10:12:42 -0400

    Build out basic server gui

--- Source Code/src/Server/ServerFrame.java
+++ Source Code/src/Server/ServerFrame.java
@@ -2,6 +2,7 @@ package Server;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 /**
  * Created by andrewsmith on 2/4/17.
@@ -9,10 +10,15 @@ import java.awt.*;
 
 class ServerFrame extends JFrame
 {
-    private static final int FRAME_WIDTH = 800;
-    private static final int FRAME_HEIGHT = 400;
+    private static final int FRAME_WIDTH = 750;
+    private static final int FRAME_HEIGHT = 375;
 
-    JTextArea textArea;
+    private JButton modifySchedulesButton;
+    private JButton closeServerButton;
+
+    private JScrollPane scrollPane;
+    private JLabel textAreaLabel;
+
     private ServerManager serverManager;
 
     ServerFrame(ServerManager sm)
@@ -29,28 +35,38 @@ class ServerFrame extends JFrame
     private void createPanel()
     {
         JPanel container = new JPanel();
-        //JPanel textAreaPanel = new JPanel();
-        //JPanel buttonPanel = new JPanel();
+        JPanel textAreaPanel = new JPanel();
+        JPanel buttonPanel = new JPanel();
 
-        //textAreaPanel.setLayout(new CardLayout());
-        //textAreaPanel.add(new JLabel("Info: "));
-        //textAreaPanel.add(textArea);
+        textAreaPanel.setLayout(new BoxLayout(textAreaPanel, BoxLayout.PAGE_AXIS));
+        textAreaPanel.add(textAreaLabel);
+        textAreaPanel.add(scrollPane);
 
-        container.setLayout(new FlowLayout(FlowLayout.CENTER));
-        container.add(textArea);
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.PAGE_AXIS));
+        buttonPanel.add(modifySchedulesButton);
+        buttonPanel.add(closeServerButton);
+
+        container.setLayout(new FlowLayout());
+        container.add(textAreaPanel);
+        container.add(buttonPanel);
 
         this.add(container);
     }
 
     private void createTextArea()
     {
-        textArea = new JTextArea(5, 20);
-        JScrollPane scrollPane = new JScrollPane(textArea);
+        JTextArea textArea = new JTextArea(20, 40);
         textArea.setEditable(false);
+        scrollPane = new JScrollPane(textArea);
+        textAreaLabel = new JLabel("Info:");
     }
 
     private void createButtons()
     {
+        modifySchedulesButton = new JButton("Modify Schedules");
+        modifySchedulesButton.setEnabled(true);
 
+        closeServerButton = new JButton("Close Server");
+        closeServerButton.setEnabled(true);
     }
 }

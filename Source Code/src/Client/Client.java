593326b4e00a4e84a6094e6f042a088f0696f85b
Tim Kelly <tkelly1996@yahoo.com>
2017-04-02 23:25:53 -0400

    add client use

--- Source Code/src/Client/Client.java
+++ Source Code/src/Client/Client.java
@@ -1,7 +1,22 @@
 package Client;
 
+import javax.swing.*;
+
 /**
  * Created by Tim on 4/2/2017.
  */
-public class Client {
+public class Client
+{
+    private JFrame parentFrame;
+
+    private String portNumber;
+    private String hostName;
+
+    public Client(String pn, String hn, JFrame pf)
+    {
+
+        this.parentFrame = pf;
+        this.portNumber = pn;
+        this.hostName = hn;
+    }
 }
